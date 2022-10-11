f77c71aff7d8e92ea8226fdd8bf7fadd37a47bd3
Rohit Ranjan <roh2110it@gmail.com>
2022-10-11 09:00:17 +0530

    Compilation Process

---
+++ Compilation/compilation.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "Hello World";
+    return 0;
+}
+// Terminal Command : g++ -Wall -save-temps compilation.cpp -o compilation
+// This will create all the temp files which are created during compilation process along with all the warnings enabled.
---
+++ Compilation/compilation.s
@@ -0,0 +1,164 @@
+	.file	"compilation.cpp"
+	.text
+	.section .rdata,"dr"
+_ZN6__pstl9execution2v1L3seqE:
+	.space 1
+_ZN6__pstl9execution2v1L3parE:
+	.space 1
+_ZN6__pstl9execution2v1L9par_unseqE:
+	.space 1
+_ZN6__pstl9execution2v1L5unseqE:
+	.space 1
+.lcomm _ZStL8__ioinit,1,1
+	.align 4
+_ZN9__gnu_cxxL21__default_lock_policyE:
+	.long	2
+	.align 4
+_ZNSt15regex_constantsL13error_collateE:
+	.space 4
+	.align 4
+_ZNSt15regex_constantsL11error_ctypeE:
+	.long	1
+	.align 4
+_ZNSt15regex_constantsL12error_escapeE:
+	.long	2
+	.align 4
+_ZNSt15regex_constantsL13error_backrefE:
+	.long	3
+	.align 4
+_ZNSt15regex_constantsL11error_brackE:
+	.long	4
+	.align 4
+_ZNSt15regex_constantsL11error_parenE:
+	.long	5
+	.align 4
+_ZNSt15regex_constantsL11error_braceE:
+	.long	6
+	.align 4
+_ZNSt15regex_constantsL14error_badbraceE:
+	.long	7
+	.align 4
+_ZNSt15regex_constantsL11error_rangeE:
+	.long	8
+	.align 4
+_ZNSt15regex_constantsL11error_spaceE:
+	.long	9
+	.align 4
+_ZNSt15regex_constantsL15error_badrepeatE:
+	.long	10
+	.align 4
+_ZNSt15regex_constantsL16error_complexityE:
+	.long	11
+	.align 4
+_ZNSt15regex_constantsL11error_stackE:
+	.long	12
+	.align 4
+_ZNSt8__detailL19_S_invalid_state_idE:
+	.long	-1
+	.def	__main;	.scl	2;	.type	32;	.endef
+.LC0:
+	.ascii "Hello World\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB9777:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__tcf_0;	.scl	3;	.type	32;	.endef
+	.seh_proc	__tcf_0
+__tcf_0:
+.LFB11575:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	leaq	_ZStL8__ioinit(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZNSt8ios_base4InitD1Ev
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	_Z41__static_initialization_and_destruction_0ii;	.scl	3;	.type	32;	.endef
+	.seh_proc	_Z41__static_initialization_and_destruction_0ii
+_Z41__static_initialization_and_destruction_0ii:
+.LFB11574:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	cmpl	$1, 16(%rbp)
+	jne	.L6
+	cmpl	$65535, 24(%rbp)
+	jne	.L6
+	leaq	_ZStL8__ioinit(%rip), %rax
+	movq	%rax, %rcx
+	call	_ZNSt8ios_base4InitC1Ev
+	leaq	__tcf_0(%rip), %rax
+	movq	%rax, %rcx
+	call	atexit
+.L6:
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	_GLOBAL__sub_I_main;	.scl	3;	.type	32;	.endef
+	.seh_proc	_GLOBAL__sub_I_main
+_GLOBAL__sub_I_main:
+.LFB11598:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	$65535, %edx
+	movl	$1, %ecx
+	call	_Z41__static_initialization_and_destruction_0ii
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section	.ctors,"w"
+	.align 8
+	.quad	_GLOBAL__sub_I_main
+	.ident	"GCC: (GNU) 11.2.0"
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt8ios_base4InitD1Ev;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt8ios_base4InitC1Ev;	.scl	2;	.type	32;	.endef
+	.def	atexit;	.scl	2;	.type	32;	.endef
+	.section	.rdata$.refptr._ZSt4cout, "dr"
+	.globl	.refptr._ZSt4cout
+	.linkonce	discard
+.refptr._ZSt4cout:
+	.quad	_ZSt4cout
