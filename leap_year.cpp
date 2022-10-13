1cfd6d01b0354d0c85e21b17c8999f2b9daeb4ce
Rohit Ranjan <roh2110it@gmail.com>
2022-10-13 13:14:18 +0530

    Leap Year

---
+++ leap_year.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int year;
+    cin >> year;
+    if (year % 4 == 0 and year % 100 != 0)
+        cout << "Leap Year";
+    else if (year % 400 == 0)
+        cout << "Leap Year";
+    else
+        cout << "Not Leap Year";
+    return 0;
+}
